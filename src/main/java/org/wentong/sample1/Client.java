13d88ce3184a2cf57435c7af7771106072af6d76
zhouwentong <zhouwentong@hualala.com>
2024-05-06 16:31:23 +0800

    1

--- src/main/java/org/wentong/sample1/Client.java
+++ src/main/java/org/wentong/sample1/Client.java
@@ -1,2 +1,23 @@
-package org.wentong.sample1;public class Client {
+package org.wentong.sample1;
+
+import java.net.Socket;
+
+/**
+ * 全连接队列溢出
+ */
+public class Client {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Started on: " + System.currentTimeMillis());
+        try {
+
+            for (int i = 0; i < 10; i++) {
+                Socket socket = new Socket("127.0.0.1", 8888);
+                System.out.println(socket.getPort());
+            }
+        } finally {
+            System.out.println("End on: " + System.currentTimeMillis());
+        }
+    }
+
 }
--- src/main/java/org/wentong/sample1/Server.java
+++ src/main/java/org/wentong/sample1/Server.java
@@ -1,2 +1,17 @@
-package org.wentong.sample1;public class Server {
+package org.wentong.sample1;
+
+import java.net.ServerSocket;
+
+/**
+ * 全连接队列溢出
+ */
+public class Server {
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket ss = new ServerSocket(8888, 2);
+        System.out.println("Started on 8888!");
+        Thread.currentThread().join();
+//        Socket accept = ss.accept();
+    }
+
 }
